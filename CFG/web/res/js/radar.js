f6a9839ac6749a3125700c43a8a429cc4941b6af
happygirlzt <happygirlzt@gmail.com>
2018-10-12 11:52:42 +0800

    Add studentlist.html

---
+++ CFG/web/res/js/radar.js
@@ -0,0 +1,28 @@
+// Radar.js
+// This chart is to show how each student feedback
+// positive and negative changes
+
+window.onload = function() {
+    var options = {
+        title: {
+            text: "Feedback of each student"
+        },
+
+        data: [{
+            type: "pie",
+            startAngle: 45,
+            showInLegend: "true",
+            legendText: "{label}",
+            indexLabel: "{label} ({y})",
+            yValueFormatString:"#,##0.#"%"",
+            dataPoints: [
+                { label: "Growth-Orientedness", y: 36 },
+                { label: "Confidence", y: 31 },
+                { label: "Strategic Thinking", y: 7 },
+                { label: "Team Orientedness", y: 7 },
+                { label: "Personal Productivity", y: 6 },
+            ]
+        }]
+    };
+    $("#chartContainer").CanvasJSChart(options);
+}
